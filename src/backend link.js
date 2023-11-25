9736e6b9f66337d71df3ae453cf7efa2e3e0a5c5
spragul <spragul33@gmail.com>
2023-11-25 22:56:54 +0530

    commit

--- src/backend link.js
+++ src/backend link.js
@@ -1,2 +1,2 @@
-//export const URL='https://e-commerce-backend-bkfh.onrender.com';
-export const URL='http://localhost:9000';
+export const URL='https://e-commerce-backend-bkfh.onrender.com';
+// export const URL='http://localhost:9000';
